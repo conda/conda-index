527f75121dffb38a9947c61f8568d9a586c9e3ab
Jannis Leidel <jannis@leidel.info>
2024-01-11 10:23:53 +0100

    Add Python 3.12.

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import nox
 @nox.session(venv_backend="conda")
 @nox.parametrize(
     "python",
-    [(python) for python in ("3.7", "3.8", "3.9", "3.10")],
+    [(python) for python in ("3.9", "3.10", "3.11", "3.12")],
 )
 def tests(session):
     session.conda_install("conda", "conda-build")
