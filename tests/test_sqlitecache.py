5a2894dae2ce594b6343066afb7484c4b42e63b7
Daniel Holth <dholth@anaconda.com>
2024-04-01 12:57:20 -0400

    improve coverage

--- tests/test_sqlitecache.py
+++ tests/test_sqlitecache.py
@@ -2,8 +2,13 @@
 Test sqlitecache
 """
 
+import json
+import sqlite3
 import tarfile
+from io import BytesIO
 from pathlib import Path
+
+import pytest
 
 from conda_index.index.common import connect
 from conda_index.index.convert_cache import (
@@ -13,7 +18,12 @@ from conda_index.index.convert_cache import (
     merge_index_cache,
     migrate,
 )
-from conda_index.index.sqlitecache import CondaIndexCache
+from conda_index.index.sqlitecache import (
+    CondaIndexCache,
+    _cache_post_install_details,
+    _cache_recipe,
+    _clear_newline_chars,
+)
 from conda_index.utils import DEFAULT_SUBDIRS
 
 
@@ -37,23 +47,74 @@ def test_cache_extract_without_stat_result(index_data):
     )
 
 
-def test_cache_device(tmp_path):
+def test_cache_unusual_files(tmp_path):
     """
-    Cover error when metadata happens to be a device file.
+    Cover error when metadata happens to be a device file; cover cache mtime
+    fallback; cover missing paths metadata.
     """
 
     (tmp_path / "noarch").mkdir()
     tar = tmp_path / "noarch" / "devicefile.tar.bz2"
 
     with tarfile.open(tar, mode="w:bz2") as t:
-        tarinfo = tarfile.TarInfo(name="info/index.json")
+        # icon file, though empty, to trigger cache-icon code. Before index.json
+        # which doesn't mention icons.
+        icon = tarfile.TarInfo(name="info/icon.png")
+        t.addfile(icon)
+        # index.json required to finish cache function
+        index = tarfile.TarInfo(name="info/index.json")
+        index.size = 2
+        t.addfile(index, BytesIO(b"{}"))
+        # device file named after metadata, to trigger an error handler
+        tarinfo = tarfile.TarInfo(name="info/paths.json")
         tarinfo.type = tarfile.CHRTYPE
         t.addfile(tarinfo)
-        t.close()
+        # will be checked by sqlite parser only
+        about = tarfile.TarInfo(name="info/about.json")
+        about.size = 8
+        t.addfile(about, BytesIO(b"not json"))
 
     cache = CondaIndexCache(tmp_path, "noarch")
 
+    with pytest.raises(sqlite3.OperationalError):
+        # XXX a malformed about.json would currently halt index processing
+        cache._extract_to_cache(cache.channel_root, cache.subdir, tar.name)
+
+    (tmp_path / "noarch" / "found").touch()
+
+    # coverage for fallback "try to stat file if not in cache"
+    found = cache.load_all_from_cache("found")
+    assert found["mtime"] > 0
+    assert cache.load_all_from_cache("notfound") == {}
+
+
+def test_cache_source_as_list(tmp_path):
+    """
+    Cover fallback when source is a list and not a dict.
+    """
+
+    (tmp_path / "noarch").mkdir()
+    tar = tmp_path / "noarch" / "source-as-list.tar.bz2"
+
+    with tarfile.open(tar, mode="w:bz2") as t:
+        # index.json required to finish cache function
+        index = tarfile.TarInfo(name="info/index.json")
+        index_data = b'{"source":["a", "b"]}'
+        index.size = len(index_data)
+        t.addfile(index, BytesIO(index_data))
+    cache = CondaIndexCache(tmp_path, "noarch")
+
+    cache.save_fs_state()
     cache._extract_to_cache(cache.channel_root, cache.subdir, tar.name)
+
+    # test load_all_from_cache still works without local file, mtime saved in
+    # save_fs_state()
+    tar.unlink()
+
+    # may or may not a correct "source" but it preserves the input
+    found = cache.load_all_from_cache(tar.name)
+    assert found["source"] == ["a", "b"]
+    assert found["mtime"] > 0
 
 
 def test_convert_legacy_cache(tmp_path):
@@ -99,3 +160,39 @@ def test_merge_index_cache(tmp_path):
             channel, subdir, _ = row[0].split("/")
             assert channel == tmp_path.name
             seen_subdirs.add(subdir)
+
+
+def test_description_as_list():
+    """
+    Rarely, the description could be a list. Or theoretically something that
+    can't be replaced into a string at all.
+    """
+    record = {"description": ["a", "list\n", "description"], "number": 0}
+    _clear_newline_chars(record, "description")
+    assert record["description"] == "alist description"
+    _clear_newline_chars(record, "number")  # coverage
+
+
+def test_cache_recipe_build_not_json_serializable():
+    """
+    Odd feature that drops build portion when recipe is not json serializable.
+    """
+    yaml_not_json = b"""
+requirements:
+    build:
+        - 2024-04-01
+"""
+    strange_recipe = BytesIO(yaml_not_json)
+    altered = _cache_recipe(strange_recipe)
+    assert altered == '{"requirements": {}}'
+
+
+def test_cache_post_install_details():
+    details = {
+        "paths": [
+            {"_path": "a", "prefix_placeholder": "x", "file_mode": "binary"},
+            {"_path": "b", "file_mode": "text"},
+            {"_path": "etc/conda/activate.d", "file_mode": "text"},
+        ]
+    }
+    _cache_post_install_details(json.dumps(details))
