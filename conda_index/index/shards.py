fd6c9bc17e21f9fe819e8f41b0c331810e817a48
Daniel Holth <dholth@anaconda.com>
2024-05-10 18:10:16 -0400

    shards cli capable of generating repodata from cache database and no packages

--- conda_index/index/shards.py
+++ conda_index/index/shards.py
@@ -2,29 +2,27 @@
 Sharded repodata.
 """
 
-import functools
+import click
 import hashlib
 import itertools
 import json
 import logging
-from concurrent.futures import ThreadPoolExecutor
-from os.path import join
 from pathlib import Path
 from typing import Any
+from . import MAX_THREADS_DEFAULT, logutil
 
 import msgpack
 import zstandard
 
 from conda_index.index.sqlitecache import CondaIndexCache
 
-from .. import utils, yaml
+from .. import yaml
 from . import (
     CONDA_PACKAGE_EXTENSIONS,
     REPODATA_VERSION,
     RUN_EXPORTS_JSON_FN,
     ChannelIndex,
     _apply_instructions,
-    _ensure_valid_channel,
 )
 
 log = logging.getLogger(__name__)
@@ -51,8 +49,8 @@ def packb_typed(o: Any) -> bytes:
 class ShardedIndexCache(CondaIndexCache):
     def index_shards(self, desired: set | None = None):
         """
-        Yield (package name, shard data) for package names in database ordered
-        by name, path i.e. filename.
+        Yield (package name, all packages with that name) from database ordered
+        by name, path i.o.w. filename.
 
         :desired: If not None, set of desired package names.
         """
@@ -77,7 +75,8 @@ class ShardedIndexCache(CondaIndexCache):
                 # hex hashes
                 shard[key][path] = pack_record(record)
 
-            yield (name, shard)
+            if not desired or name in desired:
+                yield (name, shard)
 
 
 class ChannelIndexShards(ChannelIndex):
@@ -85,8 +84,17 @@ class ChannelIndexShards(ChannelIndex):
     Sharded repodata per CEP proposal.
     """
 
-    def __init__(self, *args, cache_class=ShardedIndexCache, **kwargs):
-        super().__init__(*args, cache_class=cache_class, **kwargs)
+    def __init__(
+        self, *args, save_fs_state=False, cache_class=ShardedIndexCache, **kwargs
+    ):
+        """
+        :param cache_only: Generate repodata based on what's in the cache,
+            without using os.listdir() to check that those packages still exist
+            on disk.
+        """
+        super().__init__(
+            *args, cache_class=cache_class, save_fs_state=save_fs_state, **kwargs
+        )
 
     def index_prepared_subdir(
         self,
@@ -234,28 +242,164 @@ class ChannelIndexShards(ChannelIndex):
         return dict(per_shard_apply_instructions()), instructions
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+@click.command(context_settings={"help_option_names": ["-h", "--help"]})
+@click.argument("dir")
+@click.option("--output", help="Output repodata to given directory.")
+@click.option(
+    "--subdir",
+    multiple=True,
+    default=None,
+    help="Subdir to index. Accepts multiple.",
+)
+@click.option(
+    "-n",
+    "--channel-name",
+    help="Customize the channel name listed in each channel's index.html.",
+)
+@click.option(
+    "--patch-generator",
+    required=False,
+    help="Path to Python file that outputs metadata patch instructions from its "
+    "_patch_repodata function or a .tar.bz2/.conda file which contains a "
+    "patch_instructions.json file for each subdir",
+)
+@click.option(
+    "--channeldata/--no-channeldata",
+    help="Generate channeldata.json.",
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--rss/--no-rss",
+    help="Write rss.xml (Only if --channeldata is enabled).",
+    default=True,
+    show_default=True,
+)
+@click.option(
+    "--bz2/--no-bz2",
+    help="Write repodata.json.bz2.",
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--zst/--no-zst",
+    help="Write repodata.json.zst.",
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--run-exports/--no-run-exports",
+    help="Write run_exports.json.",
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--compact/--no-compact",
+    help="Output JSON as one line, or pretty-printed.",
+    default=True,
+    show_default=True,
+)
+@click.option(
+    "--current-index-versions-file",
+    "-m",
+    help="""
+        YAML file containing name of package as key, and list of versions as values.  The current_index.json
+        will contain the newest from this series of versions.  For example:
 
-    rss = False
-    channeldata = False
-    current_index_versions_file = None
-    patch_generator = None
-    dir = Path(__file__).parents[2] / "tests" / "index_data" / "packages"
-    output = dir.parent / "shards"
-    assert dir.exists(), dir
+        python:
+          - 3.8
+          - 3.9
+
+        will keep python 3.8.X and 3.9.Y in the current_index.json, instead of only the very latest python version.
+        """,
+)
+@click.option(
+    "--base-url",
+    help="""
+        If packages should be served separately from repodata.json, URL of the
+        directory tree holding packages. Generates repodata.json with
+        repodata_version=2 which is supported in conda 24.5.0 or later.
+        """,
+)
+@click.option(
+    "--save-fs-state/--no-save-fs-state",
+    help="""
+        Skip using listdir() to refresh the set of available packages. Used to
+        generate complete repodata.json from cache only when packages are not on
+        disk.
+        """,
+    default=False,
+    show_default=True,
+)
+@click.option(
+    "--upstream-stage",
+    help="""
+    Set to 'clone' to generate example repodata from conda-forge test database.
+    """,
+    default="fs",
+)
+@click.option("--threads", default=MAX_THREADS_DEFAULT, show_default=True)
+@click.option(
+    "--verbose",
+    help="""
+        Enable debug logging.
+        """,
+    default=False,
+    is_flag=True,
+)
+def cli(
+    dir,
+    patch_generator=None,
+    subdir=None,
+    output=None,
+    channeldata=False,
+    verbose=False,
+    threads=None,
+    current_index_versions_file=None,
+    channel_name=None,
+    bz2=False,
+    zst=False,
+    rss=False,
+    run_exports=False,
+    compact=True,
+    base_url=None,
+    save_fs_state=False,
+    upstream_stage="fs",
+):
+    logutil.configure()
+    if verbose:
+        logging.getLogger("conda_index.index").setLevel(logging.DEBUG)
+
+    if output:
+        output = Path(output).expanduser()
+
     channel_index = ChannelIndexShards(
-        dir.expanduser(),
-        channel_name=dir.name,
+        Path(dir).expanduser(),
+        channel_name=channel_name,
         output_root=output,
-        subdirs=None,
-        write_bz2=False,
-        write_zst=False,
+        subdirs=subdir,
+        write_bz2=bz2,
+        write_zst=zst,
         threads=1,
-        write_run_exports=True,
-        compact_json=True,
-        base_url=None,
+        write_run_exports=run_exports,
+        compact_json=compact,
+        base_url=base_url,
+        save_fs_state=save_fs_state,
     )
+
+    if save_fs_state is False:
+        # We call listdir() in save_fs_state, or its remote fs equivalent; then
+        # we call changed_packages(); but the changed_packages query against a
+        # remote filesystem is different than the one we need for a local
+        # filesystem. How about skipping the extract packages stage entirely by
+        # returning no changed packages? Might fail if we use
+        # threads/multiprocessing.
+        def no_changed_packages(self, *args):
+            return []
+
+        ShardedIndexCache.changed_packages = no_changed_packages
+
+    ShardedIndexCache.upstream_stage = upstream_stage
 
     current_index_versions = None
     if current_index_versions_file:
@@ -269,4 +413,9 @@ if __name__ == "__main__":
     )
 
     if channeldata:  # about 2 1/2 minutes for conda-forge
+        # XXX wants to read repodata.json, not shards
         channel_index.update_channeldata(rss=rss)
+
+
+if __name__ == "__main__":
+    cli()
