8547957ae417bc3ca0ad2d4445356b6ac5271307
Daniel Holth <dholth@anaconda.com>
2026-01-28 12:15:57 -0500

    filter by database_prefix when generating repodata from PsqlCache

--- tests/test_psql.py
+++ tests/test_psql.py
@@ -68,6 +68,15 @@ def test_repodata_paths():
     ]
 
 
+# conda-index is expected to attempt to index, but skip these files.
+INVALID_PACKAGES_IN_SUITE = {
+    "subfolder.tar.bz2",
+    "a.tar.bz2",
+    "subfolder2.tar.bz2",
+    "b.tar.bz2",
+}
+
+
 def test_psql_channel_separation(
     tmp_path: Path, index_data: Path, archives_data: Path, postgresql_database
 ):
@@ -77,12 +86,14 @@ def test_psql_channel_separation(
     """
     output_base = tmp_path / "output"
 
+    # Twice so that combined packages are written to first output if the bug
+    # occurs:
     for _ in range(2):
         for channel_root in (index_data, archives_data):
             # tmp_path is the same as used for the fixtures, contains the
             # index_data, archives_data directories
             output = output_base / channel_root.name  # default same as channel
-            output.mkdir(parents=True)
+            output.mkdir(parents=True, exist_ok=True)
 
             channel_index = ChannelIndex(
                 channel_root,
@@ -103,21 +114,18 @@ def test_psql_channel_separation(
                 patch_generator=None, current_index_versions=None, progress=False
             )
 
-            channel_index.update_channeldata(rss=True)
-
             for subdir in output.glob("*"):
                 if not subdir.is_dir():
                     continue
-                expected_packages = set(
-                    p.name
-                    for p in (channel_root / subdir.name).glob("*")
-                    if p.name.endswith((".tar.bz2", ".conda"))
+                expected_packages = (
+                    set(
+                        p.name
+                        for p in (channel_root / subdir.name).glob("*")
+                        if p.name.endswith((".tar.bz2", ".conda"))
+                    )
+                    - INVALID_PACKAGES_IN_SUITE
                 )
                 actual_packages = set(
                     repodata_paths(json.loads((subdir / "repodata.json").read_text()))
                 )
-                assert (
-                    actual_packages <= expected_packages
-                )  # modulo corrupted / "not a package" archives
-
-    print("Done")
+                assert actual_packages == expected_packages
