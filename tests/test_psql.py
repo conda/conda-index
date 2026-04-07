f11128b87768ee1e79979d996d7597be35bcc916
Daniel Holth <dholth@anaconda.com>
2026-04-07 12:55:51 -0400

    fix indentation

--- tests/test_psql.py
+++ tests/test_psql.py
@@ -315,15 +315,17 @@ def test_psql_skip_unknown_extension(tmp_path: Path):
         DummyResult("package", "package-1.0.notconda", {}, {}),
         DummyResult("package", "package-1.0.conda", {}, {"weak": ["zlib"]}),
         DummyResult("package", "package-1.0.tar.bz2", {}, {}),
-]
-shards = list(cache.indexed_shards())
-assert len(shards) == 1
-shards0 = shards[0]
-name, data = shards0
-assert name == "package"
-assert len(data["packages"]) == 1
-assert len(data["packages.conda"]) == 1
-    assert data["packages.conda"]["package-1.0.conda"]["run_exports"] == {"weak": ["zlib"]}
+    ]
+    shards = list(cache.indexed_shards())
+    assert len(shards) == 1
+    shards0 = shards[0]
+    name, data = shards0
+    assert name == "package"
+    assert len(data["packages"]) == 1
+    assert len(data["packages.conda"]) == 1
+    assert data["packages.conda"]["package-1.0.conda"]["run_exports"] == {
+        "weak": ["zlib"]
+    }
 
     indexed_packages = cache.indexed_packages()
     assert len(indexed_packages.packages) == 1
